4640d1275d73a4e7b7c4a0833a3e4fb3e5f63906
6704062610020 <s6704062610020@email.kmutnb.ac.th>
2024-11-28 14:32:53 +0700

    update 2

---
+++ Untitled1.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+	int num1 = 5;
+	int num2 = 10;
+	
+	if (num1>num2) {
+		printf("num1 is greater than num2");
+	}
+	
+	else (num2 > num1) {
+		printf("num2 is greater than num1");
+	}
+	
+	return 0;
+}
